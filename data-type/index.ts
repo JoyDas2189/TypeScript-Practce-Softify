00664260ee87c301be39a352cc27115a5b2cdc17
Joy Das <dasjoy365d@gmail.com>
2024-08-04 12:29:36 +0600

    data type added

---
+++ data-type/index.ts
@@ -0,0 +1,44 @@
+/* 
+TypeScript Type => 
+    1. Static.
+    2. Generic
+    3. Decorator.
+
+Static => 
+    1. Build in.
+    2. User Defined.
+
+    Build In => 
+        1. String.
+        2. Number.
+        3. Boolean
+        4. Undefined
+        5. NULL
+        6. VOID
+
+    User Defined => 
+        1. Array
+        2. Tuples
+        3. Function
+        4. Enum
+        5. Class
+        6. Interface
+*/
+
+// Build In Types
+
+let userId:number = 201;
+let firstName:string = "Joy";
+let lastName:string = "Das";
+let isActive:boolean = true;
+let gender:any = "Male"
+let fullName:string = firstName + " "+lastName;
+
+console.log(userId);
+console.log(firstName);
+console.log(lastName);
+console.log(isActive);
+console.log(gender);
+console.log(fullName);
+
+
