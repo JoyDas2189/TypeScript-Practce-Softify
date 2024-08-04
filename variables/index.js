5fca64df51a6382d96cad0c016bef59f13083d57
Joy Das <dasjoy365d@gmail.com>
2024-08-04 12:43:53 +0600

    variable added

---
+++ variables/index.js
@@ -0,0 +1,9 @@
+// var
+var myNumber = 201;
+myNumber = 101;
+console.log(myNumber);
+function displayer() {
+    var msg = "TypeScript";
+    return msg;
+}
+console.log(displayer());
---
+++ variables/index.ts
@@ -0,0 +1,13 @@
+// var
+
+var myNumber: number = 201;
+myNumber = 101;
+
+console.log(myNumber);
+
+function displayer () {
+    var msg = "TypeScript";
+    return msg;
+}
+
+console.log(displayer());
