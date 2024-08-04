774bce7bbd29eac5d3d895a36faa1d3a56cc254e
Joy Das <dasjoy365d@gmail.com>
2024-08-04 15:12:19 +0600

    variable added

--- variables/index.js
+++ variables/index.js
@@ -7,3 +7,18 @@ function displayer() {
     return msg;
 }
 console.log(displayer());
+//let
+var a = 10;
+// let a = 25; // not possible
+function displayer1() {
+    var msg1 = "TypeScript1";
+    return msg1;
+}
+console.log(displayer1());
+var b = 10;
+// const b = 10//not possible
+function displayer2() {
+    var msg2 = "TypeScirpt2";
+    return msg2;
+}
+console.log(displayer2());
--- variables/index.ts
+++ variables/index.ts
@@ -11,3 +11,25 @@ function displayer () {
 }
 
 console.log(displayer());
+
+
+//let
+
+let a = 10;
+// let a = 25; // not possible
+
+function displayer1 () {
+    let msg1 = "TypeScript1";
+    return msg1;
+}
+console.log(displayer1());
+
+const b = 10;
+// const b = 10//not possible
+
+function displayer2 () {
+    const msg2 = "TypeScirpt2";
+    return msg2;
+}
+console.log(displayer2());
+
